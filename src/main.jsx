359111ad1678ddf8ef0bd8338219388fe8a09260
Harshit Raj <rajharshit789@gmail.com>
2024-07-20 23:31:54 +0530

    react-app-deploy

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MovieList from './components/MovieList/MovieList';
+import Navbar from './components/Navbar/Navbar';
+import './index.css';
+import Home from './pages/homepage/home';
+import MoviePage from './pages/movie/movie';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/*' element={<h1>Error</h1>} />
+        <Route path="movie/:id" element={<MoviePage />} />
+        <Route path="movies/:type" element={<MovieList />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/pages/homepage/home.jsx
@@ -0,0 +1,15 @@
+import MovieList from "../../components/MovieList/MovieList";
+import Slider from "../../components/Slider/Slider";
+import Card from "../../components/Card/Card";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Slider />
+            <MovieList/>
+        </div>
+    )
+}
+
+export default Home;
